783ee6ab57294f03a76f746fa7bee95fa63271d0
Richadx23 <00025325@uca.edu.sv>
2025-06-06 19:28:58 -0600

    if2

---
+++ if2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int contrasena=123, intento;
+    cout << "por favor ingrese la contraseña"<< endl;
+    cin >> intento;
+
+    if (intento == contrasena)
+    {
+        cout << "puede continuar " << endl;
+    }
+
+    else 
+    {
+        cout << "contraseña incorrecta " << endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+
+
+    
+
+
+
